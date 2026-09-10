13d4fa8d211deea01f7d012c35e717af49c21fa0
tomasz <csharp@406-00>
2026-09-10 14:12:56 +0200

    q

---
+++ P03WeatherForecastWPF.Client/Services/IMeteoService.cs
@@ -0,0 +1,14 @@
+﻿using P03WeatherForecastWPF.Client.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03WeatherForecastWPF.Client.Services
+{
+    internal interface IMeteoService
+    {
+        Task<City[]> GetLocationsAsync(string locationName);
+
+        Task<Weather> GetCurrentConditionsAsync(double latitude, double longitude);
+    }
+}
