7cb6ad0f2bfd0267fd08324da4fb7c72e62b3bb6
tomasz <csharp@406-00>
2026-09-10 11:10:33 +0200

    q

--- P05Shop.API/Controllers/WeatherForecastController.cs
+++ P05Shop.API/Controllers/WeatherForecastController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using P05Shop.API.DTO;
 
 namespace P05Shop.API.Controllers
 {
@@ -12,7 +13,7 @@ namespace P05Shop.API.Controllers
         ];
 
         //https://localhost:7127/api/WeatherForecast
-  
+
         [HttpGet(Name = "GetWeatherForecast")]
         public IEnumerable<WeatherForecast> Get()
         {
@@ -63,7 +64,7 @@ namespace P05Shop.API.Controllers
 
         //https://localhost:7127/api/WeatherForecast/test/hello?number=3
         [HttpGet("test/{routeParam}")]
-        public string GetValueFromPatch([FromQuery] int number,[FromRoute] string routeParam)
+        public string GetValueFromPatch([FromQuery] int number, [FromRoute] string routeParam)
         {
             return $"Route parameter: {routeParam}, Query parameter: {number}";
         }
@@ -76,5 +77,87 @@ namespace P05Shop.API.Controllers
         }
 
 
-    } 
+
+        //https://localhost:7127/api/WeatherForecast/filter?name=New%20York&country=USA
+        [HttpGet("complexfilter")]
+        public string GetValueFromPatch([FromQuery] CityDto cityDto)
+        {
+            return $"City: {cityDto.Name}, Country: {cityDto.Country}";
+        }
+
+
+        //https://localhost:7127/api/WeatherForecast
+        [HttpPost]
+        public string AddNewCity([FromBody] CityDto cityDto)
+        {
+            return $"City: {cityDto.Name}, Country: {cityDto.Country}";
+        }
+
+        // GET  - pobieranie danych
+        // POST - dodawanie danych
+        // PUT - aktualizacja danych
+        // Delete - usuwanie danych
+
+        //https://localhost:7127/api/WeatherForecast/newCity
+        //https://localhost:7127/api/WeatherForecast/addCity
+        [HttpPost("newCity")]
+        [HttpPost("addCity")]
+        public string MultipleRoutes([FromBody] CityDto cityDto)
+        {
+            return $"City: {cityDto.Name}, Country: {cityDto.Country}";
+        }
+
+        // zwracanie statusu kodów HTTP
+
+        //https://localhost:7127/api/WeatherForecast/addNewCity
+        [HttpPost("addNewCity")]
+        public IActionResult AddCityWithStatusCode([FromBody] CityDto cityDto)
+        {
+            // return StatusCode(201, $"City: {cityDto.Name}, Country: {cityDto.Country} added successfully.");
+
+            // return Ok($"City: {cityDto.Name}, Country: {cityDto.Country} added successfully.");
+
+
+            if (cityDto.Name == null)
+            {
+                return BadRequest("name is required");
+            }
+
+
+            try
+            {
+                // proboa dodania do bazy danych
+
+            }
+            catch (Exception ex)
+            {
+                //ex.Message
+                return StatusCode(500, ex.Message);
+            }
+
+
+            int id = 4;
+
+            return Created($"https://localhost:7127/api/WeatherForecast/GetCity/{id}", $"City: {cityDto.Name}, Country: {cityDto.Country} added successfully.");
+        }
+
+        [HttpGet("GetCity/{id}")]
+        public IActionResult GetCity([FromRoute] int id)
+        {
+            // pobranie miasta z bazy danych na podstawie id
+            CityDto city = new CityDto
+            {
+                Name = "New York",
+                Country = "USA"
+            };
+            return Ok(city);
+        }
+
+        //https://localhost:7127/api/WeatherForecast/cokowiek
+        [HttpGet("[action]")]
+        public IActionResult MyDynamincMathodName()
+        {
+            return Ok("This is a dynamic method name based on the action name.");
+        }
+    }
 }
